68fe4d91b1c92f5d9b0f1df2d57f1f6d9c927b7d
whattheearl <earl.jonathan@gmail.com>
2021-03-24 23:26:38 -0400

    add skills data

---
+++ src/data/skills.ts
@@ -0,0 +1,3 @@
+export const frontend = ['Angular', 'React', 'Redux', 'RxJS', 'Typescript', 'Javascript', 'HTML', 'CSS'];
+export const backend = ['.NET', 'Express', 'OpenID Connect', 'OAuth2', 'Node', 'C#'];
+export const other = ['NPM', 'Docker', 'Linux', 'Git'];
