810ca84c7177ae89ad41fff0b6751fc97594287e
whattheearl <earl.jonathan@gmail.com>
2021-03-28 20:34:49 -0400

    resize for desktop

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import '../styles/global.css';
 
 export const Layout = styled.main`
-  max-width: 700px;
+  max-width: 1000px;
   margin: 0 auto;
   height: 100vh;
 `;
