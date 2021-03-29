dddb27953252b922b6cdf9e85bfe9ed81fc8262d
whattheearl <earl.jonathan@gmail.com>
2021-03-28 20:34:59 -0400

    add project data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,3 +1,28 @@
+export type Project = {
+    name: string,
+    imgUrl: string,
+    description: string,
+    demoUrl: string,
+    githubUrl: string,
+    technologies: string[],
+};
 
-export type Project = Array<{name: string}> ;
-export const projects: Project = [{name: 'projectName'}];
+
+export const projects: Project[] = [
+    {
+        name: 'fig',
+        imgUrl: undefined,
+        description: 'An instagram clone',
+        demoUrl: undefined,
+        githubUrl: undefined,
+        technologies: [],
+    },
+    {
+        name: 'jonathanearl.io',
+        imgUrl: undefined,
+        description: 'My current portfolio',
+        demoUrl: '/',
+        githubUrl: 'https://github.com/whattheearl/jonathanearl.io',
+        technologies: [],
+    },
+];
