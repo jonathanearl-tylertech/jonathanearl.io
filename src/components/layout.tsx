5f6e70896a65862984036559e4e84f4cae9cb0f8
whattheearl <earl.jonathan@gmail.com>
2021-04-10 14:35:25 -0700

    fix background images

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import '../styles/global.css';
 
 export const Layout = styled.main`
-  max-width: 900px;
   margin: 0 auto;
   height: 100vh;
 `;
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import { frontend, backend, other } from '../data/skills';
 import { SkillsCard } from '../components/skills-card';
 import '../styles/index.scss';
 
 export const Skills = () => {
   return (
-    <section className="skills__section">
+    <Section className="skills__section">
         <div className="skills__container">
             <SkillsCard title={"Front-end"} skills={frontend} index={1}></SkillsCard>
             <SkillsCard title={"Back-end"} skills={backend} index={2}></SkillsCard>
             <SkillsCard title={"Other"} skills={other} index={3}></SkillsCard>
         </div>
-    </section>
+    </Section>
   );
 }
+
+const Section = styled.section`
+  margin-bottom: 100px;
+`;
