84ad0012995600e5716478c21ab101d7e1375392
Jonathan Earl <earl.jonathan@gmail.com>
2021-04-11 16:54:26 -0700

    Fix: side scroll bug (#25)

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,3 +1,3 @@
-export const frontend = ['Angular, RxJS', 'React', 'Redux', 'Typescript, Javascript', 'HTML, CSS'];
-export const backend = ['.NET, C#', 'Express, Node', 'OpenID Connect', 'OAuth2'];
+export const frontend = ['Angular', 'RxJS', 'React', 'Redux', 'Typescript', 'Javascript', 'HTML, CSS'];
+export const backend = ['.NET, C#', 'Express', 'Node', 'OpenID Connect', 'OAuth2'];
 export const other = ['NPM', 'Docker', 'Linux', 'Git'];
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,7 +2,7 @@ export const colors = {
   yellow: '#F5DF4D',
   white: 'rgb(204, 214, 246)',
   navy: 'rgb(17, 34, 64)',
-  grey: 'rgb(168, 178, 209)',
+  grey: '#939597',
   green: '#33ff33',
   black: '#333',
 }
