952c26488a4217eb63472b40d1814be42919c214
whattheearl <earl.jonathan@gmail.com>
2021-04-11 17:57:35 -0700

    typo

--- src/data/intro.ts
+++ src/data/intro.ts
@@ -1,4 +1,4 @@
 export default {
   note: "This site is currently under construction.",
-  description: "I'm a seattle based software engineer who specializes in building cloud applications. I'm currently an engineer at Tyler Technologies bringing our applications from on premise to the cloud."
+  description: "I'm a Seattle based software engineer who specializes in building cloud applications. I'm currently an engineer at Tyler Technologies bringing our applications from on premise to the cloud."
 }
