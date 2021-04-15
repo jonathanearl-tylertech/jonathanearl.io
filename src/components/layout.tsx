471c2f4010197a59b6dce17977c453f531abe65d
whattheearl <earl.jonathan@gmail.com>
2021-04-14 19:40:40 -0700

    clean up layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
+import { colors } from '../styles/colors';
 import '../styles/global.css';
 
 export const Layout = styled.main`
+  /* background-color: #ccc;
+  padding: 100px 0; */
 `;
