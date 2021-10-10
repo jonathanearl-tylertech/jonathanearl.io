322953ea62afa63e603a26d4bc634a5051d75018
whattheearl <earl.jonathan@gmail.com>
2021-10-09 17:27:55 -0700

    feat: make profile card responsive

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -3,11 +3,11 @@ import { PROFILE } from '../data/PROFILE';
 
 export const ProfileCard = () => {
   return (
-    <section className="flex flex-col mb-8">
-      <div className="flex w-full mb-8">
-        <img className="h-full w-full" src={PROFILE.img} alt="user profile" />
+    <section className="flex flex-col mb-8 sm:flex-row sm:p-4 sm:content-center sm:justify-center md:p-16">
+      <div className="flex w-full h-full mb-8 sm:mb-0">
+        <img className="h-full w-full max-w-36" src={PROFILE.img} alt="user profile" />
       </div>
-      <div className="px-4">
+      <div className="px-4 sm:flex sm:flex-col sm:flex-grow sm:justify-center md:px-16">
         <div className="mb-4 text-gray-700 text-2xl capitalize font-bold">{PROFILE.firstName} {PROFILE.lastName}</div>
         <div className="mb-2 text-gray-700 text-sm font-semibold">{PROFILE.title}</div>
         <div className="mb-2 text-gray-500">{PROFILE.summary}</div>
